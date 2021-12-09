a799ebad5397e19cc900cd2100d5ddcb3293275e
scoomboot <emoessne@gmail.com>
2021-12-09 14:30:59 -0800

    Cleaned up code / white spaces. K & R style functions. used static const.

--- Calendar/main.cpp
+++ Calendar/main.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
-#include <string>
 
 // true if leap year
-bool leap_year(int year)
-{
+bool leap_year(int year) {
 	if (year % 4 == 0 && year%100!=0)
 	{
 		return true;
@@ -13,50 +11,37 @@ bool leap_year(int year)
 }
 
 // enter a year and calc next leap year.
-int next_leap(int year)
-{
-	while (!leap_year(year))
-	{
-		year++;
-	}
-	return year;
-}
+int next_leap(int year) { while (!leap_year(year)) { year++; } return year; }
 
-int number_of_leap_years(int currentYear, int birthYear)
-{
+int number_of_leap_years(int currentYear, int birthYear){
 	// born on leap year ?
-	if (leap_year(birthYear))
-	{
+	if (leap_year(birthYear)) {
 		return (currentYear - birthYear) / 4;
 	}
-	else
-	{
+	else {
 		int nextLeap = next_leap(birthYear);
 		return (currentYear - nextLeap) / 4;
 	}
 }
 
-int days_in_month(int month, int year)
-{
+int days_in_month(int month, int year) {
 	bool isLeap{ false };
 	isLeap = leap_year(year);
 	int month_days[13]{ 0,31,28,31,30,31,30,31,31,30,31,30,31};
 	int month_days_leap[13]{ 0,31,29,31,30,31,30,31,31,30,31,30,31 };
 
-	if (isLeap)
-	{
+	if (isLeap) {
 		return month_days_leap[month];
 	}
 	else
 		return month_days[month];
 }
 
-int birth_year_days_old(int year, int month, int day)
-{
+int birth_year_days_old(int year, int month, int day){
+
 	int month_days[13]{ 0,31,28,31,30,31,30,31,31,30,31,30,31 };
 	int month_days_leap[13]{ 0,31,29,31,30,31,30,31,31,30,31,30,31 };
-	if (leap_year(year))
-	{
+	if (leap_year(year)) {
 		int days{ 0 };
 		
 		// i < 13 will go over the end of the array
@@ -67,8 +52,7 @@ int birth_year_days_old(int year, int month, int day)
 		
 		return days + (month_days_leap[month] - day);
 	}
-	else
-	{
+	else {
 		int days{ 0 };
 
 		// i < 13 will go over the end of the array?
@@ -80,13 +64,11 @@ int birth_year_days_old(int year, int month, int day)
 		return days + (month_days[month] - day);
 	}
 }
-int remaining_days_in_current_month(int month, int day, int year)
-{
+int remaining_days_in_current_month(int month, int day, int year) {
 	int month_days[13]{ 0,31,28,31,30,31,30,31,31,30,31,30,31 };
 	int month_days_leap[13]{ 0,31,29,31,30,31,30,31,31,30,31,30,31 };
 	
-	if (leap_year(year))
-	{
+	if (leap_year(year)) {
 		return month_days_leap[month] - day;
 	}
 	else
@@ -95,15 +77,14 @@ int remaining_days_in_current_month(int month, int day, int year)
 
 int main()
 {
-	int days_in_leap_year{ 366 };
-	int days_in_nonleap_year{ 365 };
+	static const int days_in_leap_year{ 366 };
+	static const int days_in_nonleap_year{ 365 };
 	int birth_year{ 0 };
 	int birth_month{ 0 };
 	int birth_day{ 0 };
 	int current_year{ 0 };
 	int current_month{ 0 };
 	int current_day{ 0 };
-
 
 	std::cout << "Enter Birth Year: " << std::endl;
 	std::cin >> birth_year;
@@ -123,9 +104,6 @@ int main()
 	int years = current_year - birth_year;
 	int totalNonLeap = years - totalLeap;
 	
-	
-	
-
 	// days in first year
 	int first_year_days = birth_year_days_old(birth_year, birth_month, birth_day); // 317
 	// days in leap years
@@ -137,13 +115,7 @@ int main()
 	// days in leap + non leap + first ?
 	int total_days_old = totalNonleapDays + totalLeapDays + first_year_days - remaining_days_current_month;
 	
-	
-	
-
-
 	std::cout << total_days_old << std::endl;
-
-
 
 	return 0;
 }

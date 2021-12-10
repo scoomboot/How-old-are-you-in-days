f10581dcd6a8ff21fdeff4f8c5de87b677f63800
scoomboot <emoessne@gmail.com>
2021-12-09 18:31:35 -0800

    First Header file :)

---
+++ Calendar/days_old.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class days_old
+{
+	static const int days_in_leap_year{ 366 };
+	static const int days_in_nonleap_year{ 365 };
+	int birth_year{ 0 };
+	int birth_month{ 0 };
+	int birth_day{ 0 };
+	int current_year{ 0 };
+	int current_month{ 0 };
+	int current_day{ 0 };
+};
